67615e2e1f5d49f086875f623ef3608ff2b78cc3
Impler <704090860@qq.com>
2016-12-13 18:00:20 +0800

    integrate swagger

---
+++ AllIn/src/main/java/cn/impler/common/swagger/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package cn.impler.common.swagger.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+	
+	@Autowired
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	/**
+	 * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+	 * framework - allowing for multiple swagger groups i.e. same code base
+	 * multiple swagger resource listings.
+	 */
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
+				apiInfo()).includePatterns(".*?");
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("AllIn APIS", "AllIn APIS",
+				"My Apps API terms of service", "My Apps API Contact Email",
+				"My Apps API Licence Type", "My Apps API License URL");
+		return apiInfo;
+	}
+
+}
