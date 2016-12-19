9994f02183074ca31a0bb55470767a58e386bc05
Impler <704090860@qq.com>
2016-12-19 17:45:51 +0800

    spring converter service

--- AllIn/src/main/java/cn/impler/common/util/DateFormatUtil.java
+++ AllIn/src/main/java/cn/impler/common/util/DateFormatUtil.java
@@ -9,18 +9,18 @@ import org.apache.commons.lang.time.FastDateFormat;
 public class DateFormatUtil {
 
 	public static String getDefaultFormtedDateStr(Date date){
-		return getFormatedDateStr(date, DateFormatUtils.ISO_DATE_FORMAT);
+		return null == date? "" : getFormatedDateStr(date, DateFormatUtils.ISO_DATE_FORMAT);
 	}
 	
 	public static String getDefaultFormatedDatetimeStr(Date date){
-		return getFormatedDateStr(date, "yyyy-MM-dd HH:mm:ss");
+		return null == date? "" : getFormatedDateStr(date, "yyyy-MM-dd HH:mm:ss");
 	}
 	
 	public static String getFormatedDateStr(Date date, String pattern){
-		return getFormatedDateStr(date, FastDateFormat.getInstance(pattern));
+		return null == date? "" : getFormatedDateStr(date, FastDateFormat.getInstance(pattern));
 	}
 	public static String getFormatedDateStr(Date date, Format format){
-		return format.format(date);
+		return null == date? "" : format.format(date);
 	}
 	
 }
