556a66b407c349e278fc5b4328601a782ee3be0f
Impler <704090860@qq.com>
2017-01-18 17:57:36 +0800

    auth business code

---
+++ AllIn/src/main/java/cn/impler/auth/dto/RequestMethodType.java
@@ -0,0 +1,19 @@
+package cn.impler.auth.dto;
+/**
+ * request method type
+ * @author impler
+ * @date 2017-01-18
+ */
+public enum RequestMethodType {
+
+	//unlimited
+	ALL, 
+	// delete request 
+	DELETE, 
+	// get request
+	GET, 
+	// post request
+	POST, 
+	// put request
+	PUT;
+}
