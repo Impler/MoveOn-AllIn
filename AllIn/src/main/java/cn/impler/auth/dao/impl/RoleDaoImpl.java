bd7c4adeeb2eb0098367aebd2c48c5de4262fb4a
Impler <704090860@qq.com>
2017-01-20 17:54:19 +0800

    remove useless code

--- AllIn/src/main/java/cn/impler/auth/dao/impl/RoleDaoImpl.java
+++ AllIn/src/main/java/cn/impler/auth/dao/impl/RoleDaoImpl.java
@@ -1,15 +1,11 @@
 package cn.impler.auth.dao.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
 import cn.impler.auth.dao.AbsAuthDao;
 import cn.impler.auth.dao.RoleDao;
 import cn.impler.auth.domain.Role;
 import cn.impler.auth.domain.dto.RoleSearchBean;
-import cn.impler.framework.mybatis.dao.dto.Pagination;
-import cn.impler.framework.mybatis.dao.plugin.PaginationUtil;
 
 /**
  * role dao implement
@@ -20,12 +16,5 @@ import cn.impler.framework.mybatis.dao.plugin.PaginationUtil;
 public class RoleDaoImpl extends 
 		AbsAuthDao<RoleDao, Role, Integer, RoleSearchBean> implements
 		RoleDao {
-
-	@Override
-	public List<Role> selectInPagination(RoleSearchBean search, Pagination page) {
-		String pageStmtId = "selectByPagination";
-		String countStmtId = "selectByPaginationCount";
-		return PaginationUtil.selectByPagination(super.getSqlSession(), pageStmtId, countStmtId, search, page);
-	}
 
 }
